3f0681baa1c336f0c65030037e45b272b591df9f
lalogf <lalogf@gmail.com>
2015-03-31 11:08:23 -0500

    adding locals for languages

--- app/controllers/pacientes_controller.rb
+++ app/controllers/pacientes_controller.rb
@@ -31,7 +31,7 @@ class PacientesController < ApplicationController
 
     respond_to do |format|
       if @paciente.save
-        format.html { redirect_to @paciente, notice: 'Paciente was successfully created.' }
+        format.html { redirect_to @paciente, notice: "El paciente " + @paciente.primer_nombre + " " + @paciente.apellido_paterno + " se ha creado exitosamente."}
         format.json { render :show, status: :created, location: @paciente }
       else
         format.html { render :new }
